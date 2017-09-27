93c30beeaf17a40b279e67fc41c350f18a6f8b6c
Dmitry <neustupov@yandex.ru>
2017-09-27 18:10:28 +0300

    edit controller(add all methods)

--- src/main/java/ru/neustupov/controller/TaskController.java
+++ src/main/java/ru/neustupov/controller/TaskController.java
@@ -1,12 +1,16 @@
 package ru.neustupov.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import ru.neustupov.model.Task;
 import ru.neustupov.servise.TaskService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TaskController {
@@ -21,5 +25,67 @@ public class TaskController {
         if(task.getId() == 0) taskService.add(task);
         else taskService.edit(task);
         return "redirect:/";
+    }
+
+    @RequestMapping("/remove/{id}")
+    public String removeTask(@PathVariable("id") int id) {
+        this.taskService.delete(id);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public String editTask(@PathVariable("id") int id, Model model) {
+        TaskController.id = id;
+        model.addAttribute("listTasks", taskService.getAllTasks());
+        return "redirect:/";
+    }
+
+    @RequestMapping("taskdata{id}")
+    public String taskData(@PathVariable("id") int id, Model model) {
+        model.addAttribute("task", taskService.getTask(id));
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/")
+    public ModelAndView listOfTasks(@RequestParam(required = false) Integer page,
+                                    @RequestParam(required = false) String taskPhoneNumber) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        if (id != 0) {
+            modelAndView.addObject("task", taskService.getTask(id));
+            id = 0;
+        } else {
+            modelAndView.addObject("task", new Task());
+        }
+
+        List<Task> tasks = null;
+        if (taskPhoneNumber == null || taskPhoneNumber.length() < 3) {
+            tasks = taskService.getAllTasks();
+        } else {
+            List<Task> tempTasks = taskService.getAllTasks();
+            tasks = new ArrayList<Task>();
+            for (Task tempTask : tempTasks) {
+                if (tempTask.getPhonenumber().toLowerCase().contains(taskPhoneNumber.toLowerCase())) {
+                    System.out.println("filter: " + tempTask);
+                    tasks.add(tempTask);
+                }
+            }
+        }
+        PagedListHolder<Task> pagedListHolder = new PagedListHolder<Task>(tasks);
+        pagedListHolder.setPageSize(7);
+        modelAndView.addObject("maxPages", pagedListHolder.getPageCount());
+
+        if (page == null || page < 1 || page > pagedListHolder.getPageCount()) {
+            page = 1;
+        }
+
+        modelAndView.addObject("page", page);
+        if (page == null || page < 1 || page > pagedListHolder.getPageCount()) {
+            pagedListHolder.setPage(0);
+            modelAndView.addObject("listTasks", pagedListHolder.getPageList());
+        } else if (page <= pagedListHolder.getPageCount()) {
+            pagedListHolder.setPage(page - 1);
+            modelAndView.addObject("listTasks", pagedListHolder.getPageList());
+        }
+        return modelAndView;
     }
 }
